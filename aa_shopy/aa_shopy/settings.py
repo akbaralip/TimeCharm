0f5adf6c8d5d8571a3b3b71b6471202a1bd5478e
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2024-03-12 10:31:36 +0530

    settings made changes

--- aa_shopy/aa_shopy/settings.py
+++ aa_shopy/aa_shopy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%x2on9ti+5=gg43paijb*&$hcmg3g$=r_9$623ek%6ku-wbgc0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']   
+ALLOWED_HOSTS = ['16.171.197.244', '0.0.0.0']   
 
 
 
@@ -63,14 +63,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'aa_shopy.urls'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://54.236.15.105",  
+    "http://16.171.197.244/",  
      "http://0.0.0.0:9090",
     "http://ec2-13-53-35-242.eu-north-1.compute.amazonaws.com",
 ]
 
 # CSRF settings
 CSRF_TRUSTED_ORIGINS = [
-    "http://54.236.15.105",
+    "http://16.171.197.244/",
     "http://0.0.0.0:9090",
     "http://ec2-13-53-35-242.eu-north-1.compute.amazonaws.com",
     # Add more trusted origins here
