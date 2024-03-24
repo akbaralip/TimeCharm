98ab181816a101accb24c01f1a84e12285a13889
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2024-03-25 03:32:44 +0530

    added test user button in signin page

--- aa_shopy/authentication/views.py
+++ aa_shopy/authentication/views.py
@@ -29,8 +29,7 @@ def test_user_signin(request):
     pass1 = "12345"
     user = authenticate(username = username, password = pass1)
     if user is not None:
-        myuser = User.objects.get(id=user)
-        login(request, myuser)
+        login(request, user)
     else:
         messages.error(request, "Username or Password incorrect")  
         return redirect('signin')
